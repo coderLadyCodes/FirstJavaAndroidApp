6a900a6cc432de112fd84f76187a1be92fa29304
jeSuisCodeuse <creativegirl@outlook.fr>
2022-03-20 21:15:35 +0100

    some changes

--- app/src/main/java/com/sam/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/sam/firstandroidapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.sam.firstandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void disable(View v) {
         v.setEnabled(false);
-
+        Log.d("success", "Button is disabled after clicking");
+        Button button = (Button) v;
+        button.setText("button clicked");
     }
 }
